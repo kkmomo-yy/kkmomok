ea8738c0f1a3beec117fc03e55866f75e786056a
e's <ecruos.allen@my.com>
2020-11-21 20:47:52 +0800

    测试2

--- src/main/java/com/ws/pj/sys/controller/SysHotelController.java
+++ src/main/java/com/ws/pj/sys/controller/SysHotelController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.ws.pj.sys.pojo.JsonResult;
 import com.ws.pj.sys.service.SysHotelService;
-//地址：http://localhost/dist/hotels
+//网站地址：http://localhost/dist/hotels
 //测试
 @Controller
 @RequestMapping("/dist/")
